1f9eb64d9d9742149654b17a57b1083cc58b8746
Nouman Ahmad <nouman.edu.67@gmail.com>
2024-05-17 18:54:02 +0500

    CRUD and Faculty

---
+++ project/TA-LD-WINDOWSFORM/Faculty.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace TA_project
+{
+	partial class Faculty
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Faculty));
+			this.label1 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.exit_label = new System.Windows.Forms.Label();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.LOGINFORM = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.NameBox1 = new System.Windows.Forms.TextBox();
+			this.EMAIL = new System.Windows.Forms.Label();
+			this.Nametxt = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.IDBox3 = new System.Windows.Forms.TextBox();
+			this.EmailBox2 = new System.Windows.Forms.TextBox();
+			this.PaswordBox4 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Bauhaus 93", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.CornflowerBlue;
+			this.label1.Location = new System.Drawing.Point(428, 181);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(149, 42);
+			this.label1.TabIndex = 46;
+			this.label1.Text = "Faculty";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(671, 530);
+			this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(125, 20);
+			this.checkBox1.TabIndex = 45;
+			this.checkBox1.Text = "Show Password";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// exit_label
+			// 
+			this.exit_label.AutoSize = true;
+			this.exit_label.BackColor = System.Drawing.Color.CornflowerBlue;
+			this.exit_label.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.exit_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.exit_label.ForeColor = System.Drawing.Color.White;
+			this.exit_label.Location = new System.Drawing.Point(407, 613);
+			this.exit_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.exit_label.Name = "exit_label";
+			this.exit_label.Size = new System.Drawing.Size(151, 24);
+			this.exit_label.TabIndex = 44;
+			this.exit_label.Text = "EXIT WINDOW";
+			this.exit_label.Click += new System.EventHandler(this.exit_label_Click);
+			// 
+			// panel4
+			// 
+			this.panel4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.panel4.Location = new System.Drawing.Point(167, 517);
+			this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(640, 1);
+			this.panel4.TabIndex = 43;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.panel3.Location = new System.Drawing.Point(167, 463);
+			this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(640, 1);
+			this.panel3.TabIndex = 39;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.panel2.Location = new System.Drawing.Point(167, 411);
+			this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(640, 1);
+			this.panel2.TabIndex = 38;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.panel1.Location = new System.Drawing.Point(167, 359);
+			this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(640, 1);
+			this.panel1.TabIndex = 37;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(449, 225);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(100, 71);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 36;
+			this.pictureBox1.TabStop = false;
+			// 
+			// LOGINFORM
+			// 
+			this.LOGINFORM.AutoSize = true;
+			this.LOGINFORM.Font = new System.Drawing.Font("Bauhaus 93", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.LOGINFORM.ForeColor = System.Drawing.Color.CornflowerBlue;
+			this.LOGINFORM.Location = new System.Drawing.Point(399, 114);
+			this.LOGINFORM.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.LOGINFORM.Name = "LOGINFORM";
+			this.LOGINFORM.Size = new System.Drawing.Size(207, 68);
+			this.LOGINFORM.TabIndex = 34;
+			this.LOGINFORM.Text = "LOG IN\r\n";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+			this.label3.Location = new System.Drawing.Point(153, 471);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(87, 36);
+			this.label3.TabIndex = 33;
+			this.label3.Text = "Password";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+			this.label2.Location = new System.Drawing.Point(204, 425);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 36);
+			this.label2.TabIndex = 32;
+			this.label2.Text = "ID";
+			// 
+			// NameBox1
+			// 
+			this.NameBox1.BackColor = System.Drawing.Color.White;
+			this.NameBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.NameBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.NameBox1.ForeColor = System.Drawing.Color.Black;
+			this.NameBox1.Location = new System.Drawing.Point(249, 334);
+			this.NameBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.NameBox1.Multiline = true;
+			this.NameBox1.Name = "NameBox1";
+			this.NameBox1.Size = new System.Drawing.Size(509, 31);
+			this.NameBox1.TabIndex = 31;
+			this.NameBox1.TextChanged += new System.EventHandler(this.NameBox1_TextChanged);
+			// 
+			// EMAIL
+			// 
+			this.EMAIL.AutoSize = true;
+			this.EMAIL.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.EMAIL.ForeColor = System.Drawing.SystemColors.Highlight;
+			this.EMAIL.Location = new System.Drawing.Point(184, 369);
+			this.EMAIL.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.EMAIL.Name = "EMAIL";
+			this.EMAIL.Size = new System.Drawing.Size(59, 36);
+			this.EMAIL.TabIndex = 30;
+			this.EMAIL.Text = "Email";
+			// 
+			// Nametxt
+			// 
+			this.Nametxt.AutoSize = true;
+			this.Nametxt.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Nametxt.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.Nametxt.Location = new System.Drawing.Point(184, 326);
+			this.Nametxt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.Nametxt.Name = "Nametxt";
+			this.Nametxt.Size = new System.Drawing.Size(60, 36);
+			this.Nametxt.TabIndex = 29;
+			this.Nametxt.Text = "Name";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.button1.Location = new System.Drawing.Point(167, 559);
+			this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(640, 50);
+			this.button1.TabIndex = 28;
+			this.button1.Text = "LOGIN";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// IDBox3
+			// 
+			this.IDBox3.BackColor = System.Drawing.Color.White;
+			this.IDBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.IDBox3.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.IDBox3.ForeColor = System.Drawing.Color.Black;
+			this.IDBox3.Location = new System.Drawing.Point(249, 430);
+			this.IDBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.IDBox3.Multiline = true;
+			this.IDBox3.Name = "IDBox3";
+			this.IDBox3.Size = new System.Drawing.Size(509, 31);
+			this.IDBox3.TabIndex = 41;
+			this.IDBox3.TextChanged += new System.EventHandler(this.IDBox3_TextChanged);
+			// 
+			// EmailBox2
+			// 
+			this.EmailBox2.BackColor = System.Drawing.Color.White;
+			this.EmailBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.EmailBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.EmailBox2.ForeColor = System.Drawing.Color.Black;
+			this.EmailBox2.Location = new System.Drawing.Point(249, 377);
+			this.EmailBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.EmailBox2.Multiline = true;
+			this.EmailBox2.Name = "EmailBox2";
+			this.EmailBox2.Size = new System.Drawing.Size(509, 31);
+			this.EmailBox2.TabIndex = 40;
+			this.EmailBox2.TextChanged += new System.EventHandler(this.EmailBox2_TextChanged);
+			// 
+			// PaswordBox4
+			// 
+			this.PaswordBox4.BackColor = System.Drawing.Color.White;
+			this.PaswordBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.PaswordBox4.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.PaswordBox4.ForeColor = System.Drawing.Color.Black;
+			this.PaswordBox4.Location = new System.Drawing.Point(249, 479);
+			this.PaswordBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.PaswordBox4.Multiline = true;
+			this.PaswordBox4.Name = "PaswordBox4";
+			this.PaswordBox4.Size = new System.Drawing.Size(509, 31);
+			this.PaswordBox4.TabIndex = 42;
+			this.PaswordBox4.TextChanged += new System.EventHandler(this.PaswordBox4_TextChanged);
+			// 
+			// Faculty
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.ClientSize = new System.Drawing.Size(993, 846);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.exit_label);
+			this.Controls.Add(this.panel4);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.LOGINFORM);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.NameBox1);
+			this.Controls.Add(this.EMAIL);
+			this.Controls.Add(this.Nametxt);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.IDBox3);
+			this.Controls.Add(this.EmailBox2);
+			this.Controls.Add(this.PaswordBox4);
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "Faculty";
+			this.Text = "Faculty";
+			this.Load += new System.EventHandler(this.Faculty_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label exit_label;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label LOGINFORM;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox NameBox1;
+		private System.Windows.Forms.Label EMAIL;
+		private System.Windows.Forms.Label Nametxt;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox IDBox3;
+		private System.Windows.Forms.TextBox EmailBox2;
+		private System.Windows.Forms.TextBox PaswordBox4;
+	}
+}
---
+++ project/TA-LD-WINDOWSFORM/Faculty.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TA_project
+{
+	public partial class Faculty : Form
+	{
+		public Faculty()
+		{
+			InitializeComponent();
+		}
+
+		private void NameBox1_TextChanged(object sender, EventArgs e)
+		{}
+		private void EmailBox2_TextChanged(object sender, EventArgs e)
+		{}
+		private void IDBox3_TextChanged(object sender, EventArgs e)
+		{}
+		private void PaswordBox4_TextChanged(object sender, EventArgs e)
+		{
+			checkBox1_CheckedChanged(sender, e);
+		}
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			if (checkBox1.Checked)
+			{
+				PaswordBox4.PasswordChar = '\0'; // Show the password
+			}
+			else
+			{
+				PaswordBox4.PasswordChar = '*'; // Hide the password 
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+			var un = NameBox1.Text;
+			string em = EmailBox2.Text;
+			string pass = PaswordBox4.Text;
+			string id = IDBox3.Text;
+			string dbo = "TA_PROJECT";
+			string table = "Faculty";
+			string tbo = "faculty_id";
+			string nme = "faculty_Name";
+			login log = new login();
+
+			bool check = log.signin(un, pass, em, id, dbo,table,tbo,nme);
+			if (check == false)
+			{
+				MessageBox.Show("False");
+			}
+			else
+			{
+				//call the faculty profile here......;;;;;;<---------------------
+				
+					Form3 fal = new Form3(int.Parse(IDBox3.Text));
+					fal.Visible = true;
+					this.Visible = false;
+			}
+
+		}
+
+		private void exit_label_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+			//this.Close();
+		}
+
+		private void Faculty_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
