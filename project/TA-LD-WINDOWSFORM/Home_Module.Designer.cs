fcb006d295ee27468745c47a77c2ca392ee2b1ff
Nouman Ahmad <nouman.edu.67@gmail.com>
2024-05-17 18:56:27 +0500

    home module

---
+++ project/TA-LD-WINDOWSFORM/Home_Module.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace TA_project
+{
+	partial class Home_Module
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home_Module));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.TITLE = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.admin_label = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.Faculty_label4 = new System.Windows.Forms.Label();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.TA_label5 = new System.Windows.Forms.Label();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.LD_label4 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.flowLayoutPanel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.panel4.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			this.panel5.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(211, 580);
+			this.panel1.TabIndex = 0;
+			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+			this.label3.Location = new System.Drawing.Point(15, 294);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(160, 27);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "FeedBackForm";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(15, 233);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(106, 27);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "About Us";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+			this.label1.Location = new System.Drawing.Point(15, 160);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(149, 27);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Privacy Policy";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.flowLayoutPanel1.AutoSize = true;
+			this.flowLayoutPanel1.BackColor = System.Drawing.Color.RoyalBlue;
+			this.flowLayoutPanel1.Controls.Add(this.TITLE);
+			this.flowLayoutPanel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+			this.flowLayoutPanel1.ImeMode = System.Windows.Forms.ImeMode.Off;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(831, 91);
+			this.flowLayoutPanel1.TabIndex = 0;
+			this.flowLayoutPanel1.UseWaitCursor = true;
+			// 
+			// TITLE
+			// 
+			this.TITLE.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.TITLE.AutoSize = true;
+			this.TITLE.BackColor = System.Drawing.Color.Transparent;
+			this.TITLE.Font = new System.Drawing.Font("Bauhaus 93", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.TITLE.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+			this.TITLE.Location = new System.Drawing.Point(3, 0);
+			this.TITLE.Name = "TITLE";
+			this.TITLE.Size = new System.Drawing.Size(642, 91);
+			this.TITLE.TabIndex = 0;
+			this.TITLE.Text = "       TA-LD SYSTEM";
+			this.TITLE.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			this.TITLE.UseWaitCursor = true;
+			this.TITLE.Click += new System.EventHandler(this.TITLE_Click);
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.RoyalBlue;
+			this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel2.Controls.Add(this.admin_label);
+			this.panel2.Controls.Add(this.pictureBox1);
+			this.panel2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.panel2.Location = new System.Drawing.Point(232, 132);
+			this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(182, 150);
+			this.panel2.TabIndex = 1;
+			this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			// 
+			// admin_label
+			// 
+			this.admin_label.AutoSize = true;
+			this.admin_label.BackColor = System.Drawing.Color.RoyalBlue;
+			this.admin_label.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.admin_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.admin_label.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.admin_label.Location = new System.Drawing.Point(47, 97);
+			this.admin_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.admin_label.Name = "admin_label";
+			this.admin_label.Size = new System.Drawing.Size(89, 29);
+			this.admin_label.TabIndex = 0;
+			this.admin_label.Text = "ADMIN";
+			this.admin_label.Click += new System.EventHandler(this.admin_label_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(49, 10);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(91, 71);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 4;
+			this.pictureBox1.TabStop = false;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.RoyalBlue;
+			this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel3.Controls.Add(this.pictureBox2);
+			this.panel3.Controls.Add(this.Faculty_label4);
+			this.panel3.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.panel3.Location = new System.Drawing.Point(569, 132);
+			this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(182, 150);
+			this.panel3.TabIndex = 2;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(43, 10);
+			this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(91, 71);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 4;
+			this.pictureBox2.TabStop = false;
+			// 
+			// Faculty_label4
+			// 
+			this.Faculty_label4.AutoSize = true;
+			this.Faculty_label4.BackColor = System.Drawing.Color.RoyalBlue;
+			this.Faculty_label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Faculty_label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.Faculty_label4.Location = new System.Drawing.Point(40, 97);
+			this.Faculty_label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.Faculty_label4.Name = "Faculty_label4";
+			this.Faculty_label4.Size = new System.Drawing.Size(89, 29);
+			this.Faculty_label4.TabIndex = 4;
+			this.Faculty_label4.Text = "Faculty";
+			this.Faculty_label4.Click += new System.EventHandler(this.Faculty_label4_Click);
+			// 
+			// panel4
+			// 
+			this.panel4.BackColor = System.Drawing.Color.RoyalBlue;
+			this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel4.Controls.Add(this.pictureBox3);
+			this.panel4.Controls.Add(this.TA_label5);
+			this.panel4.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.panel4.Location = new System.Drawing.Point(232, 347);
+			this.panel4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(182, 150);
+			this.panel4.TabIndex = 3;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(49, 20);
+			this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(91, 71);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox3.TabIndex = 5;
+			this.pictureBox3.TabStop = false;
+			// 
+			// TA_label5
+			// 
+			this.TA_label5.AutoSize = true;
+			this.TA_label5.BackColor = System.Drawing.Color.RoyalBlue;
+			this.TA_label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.TA_label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.TA_label5.Location = new System.Drawing.Point(72, 97);
+			this.TA_label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.TA_label5.Name = "TA_label5";
+			this.TA_label5.Size = new System.Drawing.Size(44, 29);
+			this.TA_label5.TabIndex = 4;
+			this.TA_label5.Text = "TA";
+			this.TA_label5.Click += new System.EventHandler(this.TA_label5_Click);
+			// 
+			// panel5
+			// 
+			this.panel5.BackColor = System.Drawing.Color.RoyalBlue;
+			this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel5.Controls.Add(this.pictureBox4);
+			this.panel5.Controls.Add(this.LD_label4);
+			this.panel5.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.panel5.Location = new System.Drawing.Point(569, 347);
+			this.panel5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new System.Drawing.Size(182, 150);
+			this.panel5.TabIndex = 2;
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+			this.pictureBox4.Location = new System.Drawing.Point(45, 20);
+			this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(88, 74);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox4.TabIndex = 4;
+			this.pictureBox4.TabStop = false;
+			// 
+			// LD_label4
+			// 
+			this.LD_label4.AutoSize = true;
+			this.LD_label4.BackColor = System.Drawing.Color.RoyalBlue;
+			this.LD_label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.LD_label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.LD_label4.Location = new System.Drawing.Point(72, 97);
+			this.LD_label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.LD_label4.Name = "LD_label4";
+			this.LD_label4.Size = new System.Drawing.Size(43, 29);
+			this.LD_label4.TabIndex = 4;
+			this.LD_label4.Text = "LD";
+			this.LD_label4.Click += new System.EventHandler(this.LD_label4_Click);
+			// 
+			// Home_Module
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(831, 580);
+			this.Controls.Add(this.panel5);
+			this.Controls.Add(this.panel4);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.flowLayoutPanel1);
+			this.Controls.Add(this.panel1);
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.Name = "Home_Module";
+			this.Text = " ";
+			this.Load += new System.EventHandler(this.Form2_Load);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.flowLayoutPanel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.panel4.ResumeLayout(false);
+			this.panel4.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			this.panel5.ResumeLayout(false);
+			this.panel5.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label TITLE;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Panel panel5;
+		private System.Windows.Forms.Label admin_label;
+		private System.Windows.Forms.Label Faculty_label4;
+		private System.Windows.Forms.Label TA_label5;
+		private System.Windows.Forms.Label LD_label4;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox4;
+	}
+}
---
+++ project/TA-LD-WINDOWSFORM/Home_Module.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.CompilerServices;
+
+namespace TA_project
+{
+	public partial class Home_Module : Form
+	{
+		public Home_Module()
+		{
+			InitializeComponent();
+			
+		}
+
+		private void Form2_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+			//RoundedPanel roundedPanel= new RoundedPanel();
+			//Admin adm= new Admin();
+			//adm.ShowDialog();
+			//Admin jumpw = new Admin();
+			//jumpw.Show();
+
+		}
+	
+		private void TITLE_Click(object sender, EventArgs e)
+		{
+			///TITLE.
+
+			//this.BringToFront();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+			//this.Close();
+		}
+
+		private void admin_label_Click(object sender, EventArgs e)
+		{
+			
+			OpenNewFormAndCloseCurrent();
+
+		}
+		private void OpenNewFormAndCloseCurrent()
+		{
+
+			// Instantiate the new form
+			Admin adminForm = new Admin();
+
+			// Show the new form
+			adminForm.Show();
+			//this.Hide();
+			// Close the current form
+			
+		}
+
+		private void Faculty_label4_Click(object sender, EventArgs e)
+		{
+			Faculty fac=new Faculty();
+			fac.Show();
+			//this.Hide();
+		}
+
+		private void TA_label5_Click(object sender, EventArgs e)
+		{
+			TA t=new TA();
+			t.Show();
+			//this.Hide();
+		}
+
+		private void LD_label4_Click(object sender, EventArgs e)
+		{
+			LD lD=new LD();
+			lD.Show();
+			//this.Hide();
+		}
+
+		//to delete the top bar..;;;;
+
+
+	}
+
+}
